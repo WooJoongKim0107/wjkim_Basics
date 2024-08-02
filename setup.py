ae28283f2dfcbe6c7fd49689288df433e6aa3155
henrik <henrik@unist.ac.kr>
2024-08-02 12:26:09 +0900

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'wjkim',  # pip list, conda list 시 보이는 이름
-    packages = find_packages(),
+    author='WooJoong Kim',
+    author_email='henrik@unist.ac.kr',
+    url='https://github.com/WooJoongKim0107/wjkim_Basics',
+
+    name='wjkim',  # pip list, conda list 시 보이는 이름
+    version='0.7',
     python_requires='>=3.9',
+    packages=find_packages(),
     install_requires=[
         'more-itertools',
-        'omegaconf',
+        #'omegaconf',
+        'numpy',
+        'matplotlib',
     ],
 )
