69b90c112128708319ab57737554ca78bce3f552
henrik <henrik@unist.ac.kr>
2024-08-27 14:57:46 +0900

    SafeGuard argparse/get_workers

--- wjkim/argparse.py
+++ wjkim/argparse.py
@@ -1,10 +1,19 @@
 from sys import argv
+from os import cpu_count
+
+
+def next_to(targets):
+    for i, x in enumerate(argv):
+        if x in targets:
+            return int(argv[i+1])
 
 
 def get_workers(strict=True):
     targets = {'-p', '--workers'}
-    for i, x in enumerate(argv):
-        if x in targets:
-            return int(argv[i+1])
-    if strict:
+    workers = next_to(targets)
+    if workers > cpu_count():
+        raise ValueError("Number of workers exceed that of CPU's ({workers} > {cpu_count()})")
+    elif workers is None and strict:
         raise ValueError("Number of workers not given")
+    else:
+        return workers
